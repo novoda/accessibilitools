dc67f004488e159d3094b8e21dba868d4c3d5a99
takecare <rui@vazteixeira.org>
2016-03-23 14:34:38 +0000

    Add two more tests

--- core/src/test/java/com/novoda/accessibility/ClosedCaptionManagerTest.java
+++ core/src/test/java/com/novoda/accessibility/ClosedCaptionManagerTest.java
@@ -8,7 +8,9 @@ import org.junit.Before;
 import org.junit.Test;
 import org.mockito.Mock;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
 import static org.mockito.MockitoAnnotations.initMocks;
 
 @TargetApi(Build.VERSION_CODES.KITKAT)
@@ -31,4 +33,19 @@ public class ClosedCaptionManagerTest {
 
         verify(mockCaptioningManager).isEnabled();
     }
+
+    @Test
+    public void whenSystemCaptioningManagerIsEnabled_thenClosedCaptioningIsEnabled() {
+        when(mockCaptioningManager.isEnabled()).thenReturn(true);
+
+        assertThat(closedCaptionManager.isClosedCaptioningEnabled()).isTrue();
+    }
+
+    @Test
+    public void whenSystemCaptioningManagerIsDisabled_thenClosedCaptioningIsDisabled() {
+        when(mockCaptioningManager.isEnabled()).thenReturn(false);
+
+        assertThat(closedCaptionManager.isClosedCaptioningEnabled()).isFalse();
+    }
+
 }
