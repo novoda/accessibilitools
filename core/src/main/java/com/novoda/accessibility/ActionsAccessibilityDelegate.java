d8e432c3f5e790cc4ed49416746f55a15798067b
Ataul Munim <hello@ataulm.com>
2016-03-30 22:15:13 +0100

    Add custom usage hints for click and long click in actions delegate

--- core/src/main/java/com/novoda/accessibility/ActionsAccessibilityDelegate.java
+++ core/src/main/java/com/novoda/accessibility/ActionsAccessibilityDelegate.java
@@ -2,18 +2,40 @@ package com.novoda.accessibility;
 
 import android.content.res.Resources;
 import android.os.Bundle;
+import android.support.annotation.StringRes;
 import android.support.v4.view.AccessibilityDelegateCompat;
 import android.support.v4.view.accessibility.AccessibilityNodeInfoCompat;
 import android.view.View;
 
+import static android.support.v4.view.accessibility.AccessibilityNodeInfoCompat.ACTION_CLICK;
+import static android.support.v4.view.accessibility.AccessibilityNodeInfoCompat.ACTION_LONG_CLICK;
+
 public class ActionsAccessibilityDelegate extends AccessibilityDelegateCompat {
+
+    private static final int NO_CUSTOM_LABEL = 0;
 
     private final Resources resources;
     private final Actions actions;
 
+    @StringRes
+    private final int customClickLabel;
+
+    @StringRes
+    private final int customLongClickLabel;
+
     public ActionsAccessibilityDelegate(Resources resources, Actions actions) {
+        this(resources, actions, NO_CUSTOM_LABEL);
+    }
+
+    public ActionsAccessibilityDelegate(Resources resources, Actions actions, @StringRes int customClickLabel) {
+        this(resources, actions, customClickLabel, NO_CUSTOM_LABEL);
+    }
+
+    public ActionsAccessibilityDelegate(Resources resources, Actions actions, @StringRes int customClickLabel, @StringRes int customLongClickLabel) {
         this.resources = resources;
         this.actions = actions;
+        this.customClickLabel = customClickLabel;
+        this.customLongClickLabel = customLongClickLabel;
     }
 
     @Override
@@ -23,6 +45,27 @@ public class ActionsAccessibilityDelegate extends AccessibilityDelegateCompat {
             String label = resources.getString(action.getLabel());
             info.addAction(new AccessibilityNodeInfoCompat.AccessibilityActionCompat(action.getId(), label));
         }
+
+        addCustomDescriptionForClickEventIfNecessary(host, info);
+        addCustomDescriptionForLongClickEventIfNecessary(host, info);
+    }
+
+    private void addCustomDescriptionForClickEventIfNecessary(View host, AccessibilityNodeInfoCompat info) {
+        if (!host.isClickable() || customClickLabel == NO_CUSTOM_LABEL) {
+            return;
+        }
+
+        String customClickLabelText = resources.getString(customClickLabel);
+        info.addAction(new AccessibilityNodeInfoCompat.AccessibilityActionCompat(ACTION_CLICK, customClickLabelText));
+    }
+
+    private void addCustomDescriptionForLongClickEventIfNecessary(View host, AccessibilityNodeInfoCompat info) {
+        if (!host.isLongClickable() || customLongClickLabel == NO_CUSTOM_LABEL) {
+            return;
+        }
+
+        String customLongClickLabelText = resources.getString(customLongClickLabel);
+        info.addAction(new AccessibilityNodeInfoCompat.AccessibilityActionCompat(ACTION_LONG_CLICK, customLongClickLabelText));
     }
 
     @Override
