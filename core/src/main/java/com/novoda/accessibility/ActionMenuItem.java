82693cadde2c8820aae073ff70a82817c330009a
Ataul Munim <hello@ataulm.com>
2018-03-31 19:55:30 +0100

    Add ActionMenuItem and ActionsMenu

---
+++ core/src/main/java/com/novoda/accessibility/ActionMenuItem.java
@@ -0,0 +1,273 @@
+package com.novoda.accessibility;
+
+import android.content.Intent;
+import android.content.res.Resources;
+import android.graphics.drawable.Drawable;
+import android.support.annotation.Nullable;
+import android.support.annotation.StringRes;
+import android.util.Log;
+import android.view.ActionProvider;
+import android.view.ContextMenu;
+import android.view.MenuItem;
+import android.view.SubMenu;
+import android.view.View;
+
+class ActionMenuItem implements MenuItem {
+
+    private final Resources resources;
+    private final int id;
+
+    private CharSequence title;
+    private boolean enabled;
+    @Nullable
+    private OnMenuItemClickListener menuItemClickListener;
+
+    ActionMenuItem(Resources resources, int id, CharSequence title) {
+        this.resources = resources;
+        this.id = id;
+        this.title = title;
+    }
+
+    @Override
+    public int getItemId() {
+        return id;
+    }
+
+    @Override
+    public int getGroupId() {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public int getOrder() {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public MenuItem setTitle(CharSequence title) {
+        this.title = title;
+        return this;
+    }
+
+    @Override
+    public MenuItem setTitle(@StringRes int title) {
+        setTitle(resources.getString(title));
+        return this;
+    }
+
+    @Override
+    public CharSequence getTitle() {
+        return title;
+    }
+
+    @Override
+    public MenuItem setTitleCondensed(@Nullable CharSequence title) {
+        if (title != null) {
+            throw new UnsupportedOperationException();
+        }
+        return this;
+    }
+
+    @Override
+    public CharSequence getTitleCondensed() {
+        return title;
+    }
+
+    @Override
+    public MenuItem setIcon(Drawable icon) {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public MenuItem setIcon(int iconRes) {
+        if (iconRes != 0) {
+            throw new UnsupportedOperationException();
+        }
+        return this;
+    }
+
+    @Override
+    public Drawable getIcon() {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public MenuItem setIntent(Intent intent) {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public Intent getIntent() {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public MenuItem setShortcut(char numericChar, char alphaChar) {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public MenuItem setNumericShortcut(char numericChar) {
+        Log.w("!!!", "setNumericShortcut(char) is not supported but is called by MenuInflater");
+        return this;
+    }
+
+    @Override
+    public char getNumericShortcut() {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public MenuItem setAlphabeticShortcut(char alphaChar) {
+        Log.w("!!!", "setAlphabeticShortcut(char) is not supported but is called by MenuInflater");
+        return this;
+    }
+
+    @Override
+    public char getAlphabeticShortcut() {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public MenuItem setCheckable(boolean checkable) {
+        if (checkable) {
+            throw new UnsupportedOperationException();
+        }
+        return this;
+    }
+
+    @Override
+    public boolean isCheckable() {
+        return false;
+    }
+
+    @Override
+    public MenuItem setChecked(boolean checked) {
+        if (checked) {
+            throw new UnsupportedOperationException();
+        }
+        return this;
+    }
+
+    @Override
+    public boolean isChecked() {
+        return false;
+    }
+
+    @Override
+    public MenuItem setVisible(boolean visible) {
+        if (!visible) {
+            throw new UnsupportedOperationException();
+        }
+        return this;
+    }
+
+    /**
+     * For our case, a menu item will always/only be visible if enabled.
+     */
+    @Override
+    public boolean isVisible() {
+        return isEnabled();
+    }
+
+    @Override
+    public MenuItem setEnabled(boolean enabled) {
+        this.enabled = enabled;
+        return this;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return enabled;
+    }
+
+    @Override
+    public boolean hasSubMenu() {
+        return false;
+    }
+
+    @Nullable
+    @Override
+    public SubMenu getSubMenu() {
+        return null;
+    }
+
+    @Override
+    public MenuItem setOnMenuItemClickListener(OnMenuItemClickListener menuItemClickListener) {
+        this.menuItemClickListener = menuItemClickListener;
+        return this;
+    }
+
+    @Nullable
+    @Override
+    public ContextMenu.ContextMenuInfo getMenuInfo() {
+        return null;
+    }
+
+    @Override
+    public void setShowAsAction(int actionEnum) {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public MenuItem setShowAsActionFlags(int actionEnum) {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public MenuItem setActionView(View view) {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public MenuItem setActionView(int resId) {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public View getActionView() {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public MenuItem setActionProvider(ActionProvider actionProvider) {
+        throw new UnsupportedOperationException();
+    }
+
+    @Nullable
+    @Override
+    public ActionProvider getActionProvider() {
+        return null;
+    }
+
+    @Override
+    public boolean expandActionView() {
+        return false;
+    }
+
+    @Override
+    public boolean collapseActionView() {
+        return false;
+    }
+
+    @Override
+    public boolean isActionViewExpanded() {
+        return false;
+    }
+
+    @Override
+    public MenuItem setOnActionExpandListener(OnActionExpandListener listener) {
+        throw new UnsupportedOperationException();
+    }
+
+    /**
+     * Returns true if action was invoked, else false.
+     */
+    boolean invokeAction() {
+        if (isEnabled() && menuItemClickListener != null) {
+            menuItemClickListener.onMenuItemClick(this);
+            return true;
+        }
+        return false;
+    }
+}
---
+++ core/src/main/java/com/novoda/accessibility/ActionsMenu.java
@@ -0,0 +1,184 @@
+package com.novoda.accessibility;
+
+import android.content.ComponentName;
+import android.content.Intent;
+import android.content.res.Resources;
+import android.support.annotation.Nullable;
+import android.support.annotation.StringRes;
+import android.util.Log;
+import android.view.KeyEvent;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.SubMenu;
+
+import java.util.ArrayList;
+import java.util.List;
+
+class ActionsMenu implements Menu {
+
+    private final Resources resources;
+    private final List<ActionMenuItem> menuItems = new ArrayList<>();
+
+    ActionsMenu(Resources resources) {
+        this.resources = resources;
+    }
+
+    @Override
+    public MenuItem add(@StringRes int titleRes) {
+        return add(resources.getString(titleRes));
+    }
+
+    @Override
+    public MenuItem add(CharSequence title) {
+        throw new UnsupportedOperationException("Items without IDs explicitly set aren't supported.");
+    }
+
+    @Override
+    public MenuItem add(int groupId, int itemId, int order, @StringRes int titleRes) {
+        return add(groupId, itemId, order, resources.getString(titleRes));
+    }
+
+    @Override
+    public MenuItem add(int groupId, int itemId, int order, CharSequence title) {
+        if (itemId == 0 || menuItemsContainsItemWithId(itemId)) {
+            throw new IllegalArgumentException("itemId for \"" + title + "\" is missing or matches another item in menu.");
+        }
+        ActionMenuItem menuItem = new ActionMenuItem(resources, itemId, title);
+        menuItems.add(menuItem);
+        return menuItem;
+    }
+
+    private boolean menuItemsContainsItemWithId(int itemId) {
+        for (ActionMenuItem menuItem : menuItems) {
+            if (menuItem.getItemId() == itemId) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    @Override
+    public SubMenu addSubMenu(CharSequence title) {
+        return addSubMenu(0, 0, 0, title);
+    }
+
+    @Override
+    public SubMenu addSubMenu(@StringRes int titleRes) {
+        return addSubMenu(resources.getString(titleRes));
+    }
+
+    @Override
+    public SubMenu addSubMenu(int groupId, int itemId, int order, @StringRes int titleRes) {
+        return addSubMenu(groupId, itemId, order, resources.getString(titleRes));
+    }
+
+    @Override
+    public SubMenu addSubMenu(int groupId, int itemId, int order, CharSequence title) {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public int addIntentOptions(int groupId, int itemId, int order, ComponentName caller, Intent[] specifics, Intent intent, int flags, MenuItem[] outSpecificItems) {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public void removeItem(int id) {
+        ActionMenuItem itemToRemove = null;
+        for (ActionMenuItem menuItem : menuItems) {
+            if (menuItem.getItemId() == id) {
+                itemToRemove = menuItem;
+                break;
+            }
+        }
+        menuItems.remove(itemToRemove);
+    }
+
+    @Override
+    public void removeGroup(int groupId) {
+        List<ActionMenuItem> itemsToRemove = new ArrayList<>();
+        for (ActionMenuItem menuItem : menuItems) {
+            if (menuItem.getGroupId() == groupId) {
+                itemsToRemove.add(menuItem);
+            }
+        }
+        menuItems.removeAll(itemsToRemove);
+    }
+
+    @Override
+    public void clear() {
+        menuItems.clear();
+    }
+
+    @Override
+    public void setGroupCheckable(int group, boolean checkable, boolean exclusive) {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public void setGroupVisible(int group, boolean visible) {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public void setGroupEnabled(int group, boolean enabled) {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public boolean hasVisibleItems() {
+        for (ActionMenuItem menuItem : menuItems) {
+            if (menuItem.isVisible()) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    @Nullable
+    @Override
+    public MenuItem findItem(int id) {
+        for (ActionMenuItem menuItem : menuItems) {
+            if (menuItem.getItemId() == id) {
+                return menuItem;
+            }
+        }
+        return null;
+    }
+
+    @Override
+    public int size() {
+        return menuItems.size();
+    }
+
+    @Override
+    public MenuItem getItem(int index) {
+        return menuItems.get(index);
+    }
+
+    @Override
+    public void close() {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public boolean performShortcut(int keyCode, KeyEvent event, int flags) {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public boolean isShortcutKey(int keyCode, KeyEvent event) {
+        return false;
+    }
+
+    @Override
+    public boolean performIdentifierAction(int id, int flags) {
+        MenuItem menuItem = findItem(id);
+        return menuItem != null && ((ActionMenuItem) menuItem).invokeAction();
+    }
+
+    @Override
+    public void setQwertyMode(boolean isQwerty) {
+        throw new UnsupportedOperationException();
+    }
+}
